22e39af0e5cfb6147d9043223831264bf539d8f3
ruiming <zhourui@streamax.com>
2022-08-22 12:39:17 +0800

    fixed bug

--- gen_quant_image_list.py
+++ gen_quant_image_list.py
@@ -4,10 +4,8 @@ import glob
 IMAGE_DIR = r'/zhourui/workspace/pro/source/caffe2rknn/config/call/data/quant_images_112'
 out_file_path = r'/zhourui/workspace/pro/source/caffe2rknn/config/call/data/quant_images_112/list.txt'
 
-prefix = 'config/call/data/quant_images_112'
-
 images_list = glob.glob(os.path.join(IMAGE_DIR, '*.jpg'))
 
 with open(out_file_path,'w') as fp:
     for imgpath in images_list:
-        fp.write(os.path.join(prefix, os.path.basename(imgpath))+'\n')
+        fp.write(os.path.basename(imgpath)+'\n')
